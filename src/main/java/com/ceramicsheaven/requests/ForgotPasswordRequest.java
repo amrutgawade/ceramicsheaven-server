a6fbf10a56cc2424b5f0c03f7113dc793b70f7ed
rutikeshsawant <rutikeshsawant1234@gmail.com>
2024-04-15 22:38:37 +0530

    fix: Product individual api, all product with filter

---
+++ src/main/java/com/ceramicsheaven/requests/ForgotPasswordRequest.java
@@ -0,0 +1,40 @@
+package com.ceramicsheaven.requests;
+
+public class ForgotPasswordRequest {
+
+    private String email;
+    private String currentPassword;
+    private String newPassword;
+
+    public ForgotPasswordRequest() {
+    }
+    public ForgotPasswordRequest(String email, String currentPassword, String newPassword) {
+        this.email = email;
+        this.currentPassword = currentPassword;
+        this.newPassword = newPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
